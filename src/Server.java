5c9fcacc0a8a50d1625b1693d3ad98c165276227
Deep <dparekh20@outlook.com>
2019-11-08 01:11:47 -0800

    Creating repo

---
+++ src/Server.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ServerSocket socket;
+		
+		try {
+
+			socket = new ServerSocket(5000);
+			
+			while(true) {
+				Socket client = socket.accept();
+				
+			}
+		}catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
